c9b83fecc7b902f8abc6d145db0732cde33b2a07
GuardianLux <unholyagent@gmail.com>
2021-02-16 17:13:14 +1100

    Initial cargo setup

---
+++ guessing_game/target/debug/deps/guessing_game-90da175e3921e4d7.d
@@ -0,0 +1,5 @@
+/home/matt-hendrie/Documents/rust-guessing-game/guessing_game/target/debug/deps/guessing_game-90da175e3921e4d7: src/main.rs
+
+/home/matt-hendrie/Documents/rust-guessing-game/guessing_game/target/debug/deps/guessing_game-90da175e3921e4d7.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/matt-hendrie/Documents/rust-guessing-game/guessing_game/target/debug/guessing_game: /home/matt-hendrie/Documents/rust-guessing-game/guessing_game/src/main.rs
